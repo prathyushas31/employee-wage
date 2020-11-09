c246cabae63aad9d708e232a846ee3635b11c633
prathyushas31 <prathyushas1111@gmail.com>
2020-11-09 11:54:19 +0530

    updated the solving using switch case statement

--- employeewage.sh
+++ employeewage.sh
@@ -1,18 +1,18 @@
-isPartTime=1;
-isFullTime=2;
-empRatePerHr=20;
-empCheck=$(( RANDOM%3 ));
+ isPartTime=1;
+ isFullTime=2;
+ empRatePerHr=20;
+ empCheck=$(( RANDOM%3 ));
 
-case $empCheck in
-	$isFullTime)
-					empHrs=8
-					;;
+ case $empCheck in
+	 $isFullTime)
+		empHrs=8
+		;;
 	$isPartTime)
-					empHrs=4
-					;;
+		empHrs=4
+		;;
 	*)
-					empHrs=0
-					;;
-esac
-salary=$(( $empHrs*$empRatePerHr ));
+	empHrs=0
+	;;
+ esac
+ salary=$(( $empHrs*$empRatePerHr ));
 
