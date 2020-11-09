f89b8eb4eab7df7ea775928a3eefe6d1643b4437
prathyushas31 <prathyushas1111@gmail.com>
2020-11-09 11:42:46 +0530

    updated the employee is present or absent

--- employeewage.sh
+++ employeewage.sh
@@ -1,9 +1,9 @@
-isPresent=1;
-randomCheck=$(( RANDOM%2 ));
-if [ $isPresent -eq $randomCheck ];
-then
-	echo "Employee is Present";
-else
-	echo "Employee is absent";
-fi
+ isPresent=1;
+ randomCheck=$(( RANDOM%2 ));
+ if [ $isPresent -eq $randomCheck ];
+ then
+   	echo "Employee is Present";
+ else
+   	echo "Employee is absent";
+ fi
 
